945a3cead1cdd104c6913f3540a4a83e161d4d85
Robson Rocha <62728249+rsrocha-2020@users.noreply.github.com>
2022-01-27 08:08:24 -0300

    Create primeiroPassos.js
    Primeiros passos com JS

---
+++ Alura/Formação-Front-End/JS-Primeiros-Passos/primeiroPassos.js
@@ -0,0 +1,16 @@
+console.log("Meu primeiro programa!");
+console.log("Trabalhando com variáveis:");
+
+const idade = 34;
+
+console.log(idade);
+
+const nome = "Robson";
+
+console.log("Meu nome é "+nome+" eu tenho "+idade+" anos de idade!");
+console.log(`Meu nome é ${nome} eu tenho ${idade} anos de idade!`);
+console.log();
+console.log("Operações Aritméricas");
+console.log();
+console.log(2+2);
+console.log("2"+"2");
